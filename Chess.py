5bdcd20af7e90a9d6a2dce31b68b2548e4c93fee
shadowas-py <81883796+shadowas-py@users.noreply.github.com>
2021-04-10 23:59:28 +0200

    Add files via upload

--- Chess.py
+++ Chess.py
@@ -9,42 +9,16 @@ pygame.init()
 
 #IMPORTS
 game = Game()
-# graphic = Graphic()
 
 #SETTINGS
 pygame.display.set_caption('Szachy')
 
-def get_game_coord_from_mouse():
-    mouse_pos = pygame.mouse.get_pos()
-    coord = ((mouse_pos[0] - BOARD_POSITION[1])//TILE_SIZE, (mouse_pos[1] - BOARD_POSITION[0])//TILE_SIZE)
-    if BOARD_END_POSITION >= mouse_pos >= BOARD_POSITION:
-        return coord
-    else:
-        return None
-
-def selecting_piece(coord):
-    row, col = coord[0], coord[1]
-    piece = game.board[col][row]
-    if piece != "--":
-        return piece
-    else:
-        return None
-
-def making_move(piece_selected, base_coord, target_coord):
-    row, col = target_coord[0], target_coord[1]
-    target_content = game.board[col][row]
-    if target_content == "--":
-        game.board[base_coord[1]][base_coord[0]] = "--" 
-        game.board[target_coord[1]][target_coord[0]] = piece_selected
-        for i in range (len(game.board)):
-            print(game.board[i])
-        print("\n")
-
 def main():
+    run = True
     clock = pygame.time.Clock()
     piece_selected = None
     coord_selected = None
-    run = True
+    active_player = 'w'
     drawing_board()
     drawing_pieces(game.board)
     while run:
@@ -54,18 +28,19 @@ def main():
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:#jezeli wcisniety LEFT MOUSE BUTTON
                 coord = get_game_coord_from_mouse()
-                if piece_selected == None :  # Wchodzi jeżeli nic nie jest zaznaczone i nie pozwala zaznaczyć pustego pola
-                    piece_selected = selecting_piece(coord)
-                    if piece_selected == '--':
-                        piece_selected = None
+                if piece_selected is None :  # Wchodzi jeżeli nic nie jest zaznaczone i nie pozwala zaznaczyć pustego pola
+                    piece_selected = selecting_piece(game.board, coord, active_player)
+                    if piece_selected is None:
                         break
                     coord_selected = coord
                 elif piece_selected != None: # Wchodzi jeżeli zaznaczona jest jakas figura
-                    making_move(piece_selected, coord_selected, target_coord=coord)
-                    drawing_pieces(game.board)   
+                    if making_move(game.board, piece_selected, coord_selected, target_coord=coord) is None:
+                        drawing_pieces(game.board)
+                    else:
+                        break
+                    active_player = switching_turns(active_player)
                     piece_selected = None
-
-                    pygame.display.update()
+                pygame.display.update()
     pygame.quit()
 
 if __name__ == "__main__":
