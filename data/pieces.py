5b13405fd20f127c2acd0b1acbbcafb30edb8287
shadowas-py <81883796+shadowas-py@users.noreply.github.com>
2021-04-13 00:23:33 +0200

    Add files via upload

--- data/pieces.py
+++ data/pieces.py
@@ -1,5 +1,23 @@
 class Pieces:
-    def __init__(self, coord, piece):
+    def __init__(self, piece, coord_piece):
+
+        self.king():
+            def king_movement()
+                movement = abs(1)
+                castling = (2,0)(1,0)
+                long_castling_flag = True
+                short_castling_flag = True
+            def castling ():
+                if move == (2,0):
+                    board(7,7) = '--'
+                    board(6,7) = self.king()
+                    board(5,7) = self.rook()
+                    rook = (king.coord[0]-1)
+                else:
+                    board(0,7) = '--'
+                    board(2,7) = self.king()
+
+
         self.pawn_moves():
             if piece[0] = "b":
                 if coord[1] != 1,7:
@@ -39,3 +57,4 @@ class Pieces:
         self.king()
             self.queen
         pass
+
