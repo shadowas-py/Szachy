283c87bb619ea3c881a76196a352f806689e7867
shadowas-py <glas1690@gmail.com>
2021-04-24 02:21:44 +0200

    skomentowane co niepotrzebne

--- data/pieces.py
+++ data/pieces.py
@@ -17,97 +17,99 @@ class Pawn(Piece):
         self.board = board
 
 
-    def building_pawn_moves_list(self):
+    def listing_pawn_moves_list(self):
         if self.piece_color == 'w':
             pawn_moves = [N]
             if self.piece_coord[1] == WHITE_PAWN_STARTING_TILE: # Jezeli pionek znajduje sie w rzedzie startowym
-                pawn_moves.extend(sum_directions(N,N))
-            if self.board(sum_directions(self.board.coord, (sum_directions(N,W)))) == 'b':
-                pawn_moves.extend(sum_directions(N,W))
-            if self.board(sum_directions(self.board.coord, (sum_directions(N,E)))) == 'b':
-                pawn_moves.extend(sum_directions(N,E))
+                pawn_moves.extend(sum_directions(N, N))
+            if self.board(sum_directions(self.board.coord, (sum_directions(N, W)))) == 'b':
+                pawn_moves.extend(sum_directions(N, W))
+            if self.board(sum_directions(self.board.coord, (sum_directions(N, E)))) == 'b':
+                pawn_moves.extend(sum_directions(N, E))
+            return pawn_moves
         elif self.piece_color == 'b':
             pawn_moves = [S]
-            if self.coord[1] == BLACK_PAWN_STARTING_TILE: # Jezeli pionek jest na polu startowym
-                pawn_moves = extend(UPx2)
-            if boord.coord + (UP + LEFT) == black
-                pawn_moves = extend(UP + LEFT)
-            if boord.coord + (UP + RIGHT) == black
-                pawn_moves = extend(UP + RIGHT)
+            if self.piece_coord[1] == WHITE_PAWN_STARTING_TILE: # Jezeli pionek znajduje sie w rzedzie startowym
+                pawn_moves.extend(sum_directions(S, S))
+            if self.board(sum_directions(self.board.coord, (sum_directions(S, W)))) == 'b':
+                pawn_moves.extend(sum_directions(S, W))
+            if self.board(sum_directions(self.board.coord, (sum_directions(S, E)))) == 'b':
+                pawn_moves.extend(sum_directions(S, E))
+            return pawn_moves
 
 
-MOVEMENT ={
-    'wP': (UP, (0, -2)),
-    'wR': (UP, RIGHT, LEFT, DOWN),
-    'wB': UP,
-    'wN': UP,
-    'wQ': UP,
-    'wK': UP,
-    'bP': (DOWN, (0, 2)),
-    'bR': UP,
-    'bN': UP,
-    'bB': UP,
-    'bQ': UP,
-    'bK': UP,
-}
-
-ONLY_CAPTURING_MOVEMENT = {
-    'wP':(UP+LEFT,UP+RIGHT),
-    'bP':(DOWN+LEFT, DOWN+RIGHT)
-}
+# MOVEMENT ={
+#     'wP': (UP, (0, -2)),
+#     'wR': (UP, RIGHT, LEFT, DOWN),
+#     'wB': UP,
+#     'wN': UP,
+#     'wQ': UP,
+#     'wK': UP,
+#     'bP': (DOWN, (0, 2)),
+#     'bR': UP,
+#     'bN': UP,
+#     'bB': UP,
+#     'bQ': UP,
+#     'bK': UP,
+# }
+#
+# ONLY_CAPTURING_MOVEMENT = {
+#     'wP':(UP+LEFT,UP+RIGHT),
+#     'bP':(DOWN+LEFT, DOWN+RIGHT)
+# }
 
 # class Piece:
 #     def __init__(self):
 #         pass
 
-def valid_pawn_moves(piece_selected, piece_coord, target_content, target_coord):  # move
-    moves_allowed = []  # TU
-    if piece_selected[0] == "w":  # JEZELI JEST BIALY
-        if piece_coord[1] < 6:  # JEZELI NIE JEST TO JEGO PIERWSZY RUCH
-            if target_content[0] == '-':  # JEZELI POLE DOCELOWE JEST PUSTE
-                moves_allowed = MOVEMENT['wP']
-                return moves_allowed
-            else:  # JEZELI POLE DOCELOWE JEST ZAJETE
-                moves_allowed = ONLY_CAPTURING_MOVEMENT['wP']
-                return moves_allowed
-        elif piece_coord[1] == 6:  # JEZELI JEST TO PIERWSZY RUCH TEJ BIERKI
-            if target_content[0] == '-':
-                print(MOVEMENT['wP'])
-                moves_allowed = tuple(MOVEMENT['wP'])
-                return moves_allowed
-            else:
-                moves_allowed = ONLY_CAPTURING_MOVEMENT['wP']
-                return moves_allowed
-        elif target_coord[1] == 0:
-            if target_content[0] != '-':
-                moves_allowed = MOVEMENT['wP']
-                return moves_allowed  # promocja
-            else:
-                moves_allowed = ONLY_CAPTURING_MOVEMENT['wP']  # promocja
-                return moves_allowed
-    elif piece_selected[0] is "b":  # JEZELI CZARNY
-        if piece_coord[1] > 1:  # JEZELI NIE JEST TO JEGO PIERWSZY RUCH
-            if target_content[0] == '--':  # JEZELI POLE DOCELOWE JEST PUSTE
-                moves_allowed = MOVEMENT['bP']
-                return moves_allowed
-            else:  # JEZELI POLE DOCELOWE JEST ZAJETE
-                moves_allowed = ONLY_CAPTURING_MOVEMENT['bP']
-                return moves_allowed
-        elif piece_coord[1] == 1:  # JEZELI JEST TO PIERWSZY RUCH TEJ BIERKI
-            if target_content[0] == '-':
-                moves_allowed = (MOVEMENT['bP'])
-                return moves_allowed
-            else:
-                moves_allowed = ONLY_CAPTURING_MOVEMENT['bP']
-                return moves_allowed
-        elif target_coord[1] == 7:
-            if target_content[0] != '-':
-                moves_allowed = MOVEMENT['bP']
-                return moves_allowed  # promocja
-            else:
-                moves_allowed = ONLY_CAPTURING_MOVEMENT['bP']  # promocja
-                return moves_allowed
-        pass
+# def valid_pawn_moves(piece_selected, piece_coord, target_content, target_coord):  # move
+#     moves_allowed = []  # TU
+#     if piece_selected[0] == "w":  # JEZELI JEST BIALY
+#         if piece_coord[1] < 6:  # JEZELI NIE JEST TO JEGO PIERWSZY RUCH
+#             if target_content[0] == '-':  # JEZELI POLE DOCELOWE JEST PUSTE
+#                 moves_allowed = MOVEMENT['wP']
+#                 return moves_allowed
+#             else:  # JEZELI POLE DOCELOWE JEST ZAJETE
+#                 moves_allowed = ONLY_CAPTURING_MOVEMENT['wP']
+#                 return moves_allowed
+#         elif piece_coord[1] == 6:  # JEZELI JEST TO PIERWSZY RUCH TEJ BIERKI
+#             if target_content[0] == '-':
+#                 print(MOVEMENT['wP'])
+#                 moves_allowed = tuple(MOVEMENT['wP'])
+#                 return moves_allowed
+#             else:
+#                 moves_allowed = ONLY_CAPTURING_MOVEMENT['wP']
+#                 return moves_allowed
+#         elif target_coord[1] == 0:
+#             if target_content[0] != '-':
+#                 moves_allowed = MOVEMENT['wP']
+#                 return moves_allowed  # promocja
+#             else:
+#                 moves_allowed = ONLY_CAPTURING_MOVEMENT['wP']  # promocja
+#                 return moves_allowed
+#     elif piece_selected[0] is "b":  # JEZELI CZARNY
+#         if piece_coord[1] > 1:  # JEZELI NIE JEST TO JEGO PIERWSZY RUCH
+#             if target_content[0] == '--':  # JEZELI POLE DOCELOWE JEST PUSTE
+#                 moves_allowed = MOVEMENT['bP']
+#                 return moves_allowed
+#             else:  # JEZELI POLE DOCELOWE JEST ZAJETE
+#                 moves_allowed = ONLY_CAPTURING_MOVEMENT['bP']
+#                 return moves_allowed
+#         elif piece_coord[1] == 1:  # JEZELI JEST TO PIERWSZY RUCH TEJ BIERKI
+#             if target_content[0] == '-':
+#                 moves_allowed = (MOVEMENT['bP'])
+#                 return moves_allowed
+#             else:
+#                 moves_allowed = ONLY_CAPTURING_MOVEMENT['bP']
+#                 return moves_allowed
+#         elif target_coord[1] == 7:
+#             if target_content[0] != '-':
+#                 moves_allowed = MOVEMENT['bP']
+#                 return moves_allowed  # promocja
+#             else:
+#                 moves_allowed = ONLY_CAPTURING_MOVEMENT['bP']  # promocja
+#                 return moves_allowed
+#         pass
 
         # self.wK = (DIRECTIONS.values())
             # castling = (2,0)(1,0)
