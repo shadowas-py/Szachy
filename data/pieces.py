daea734020b7b06e68e427ddcacde3611f71ddfb
shadowas-py <glas1690@gmail.com>
2021-04-25 01:36:21 +0200

    reformat file pieces.py i dopisanie klasy Knight

--- data/pieces.py
+++ data/pieces.py
@@ -1,5 +1,6 @@
 from .constants import N, S, W, E, GRID_SIZE
 
+
 # Może wychodzić po za skale/plansze gry
 def sum_directions(direction1, direction2, direction3=(0, 0)):
     return tuple(map(sum, zip(direction1, direction2, direction3)))
@@ -16,7 +17,8 @@ class Pawn(Piece):
 
     def __init__(self, board, piece_selected, piece_coord):  ### ///////////////////////////////////
         super().__init__(piece_selected, piece_coord)
-        self.movement_range = 1           # domyslna ilosc pól o jakie dana figura moze sie poruszac
+        self.movement_range = 1  # domyslna ilosc pól o jakie dana figura moze sie poruszac
+
         # Kolizje z innymi bierkami beda liczone gdzie indziej
         def listing_pawn_moves():
             if self.piece_color == 'w':
@@ -25,13 +27,13 @@ class Pawn(Piece):
                 if self.piece_coord[1] == white_pawn_starting_row:  # Jezeli pionek znajduje sie w rzedzie startowym
                     pawn_moves.extend(sum_directions(N, N))
                 new_coord = sum_directions(self.piece_coord, (sum_directions(N, W)))
-                if board[new_coord[1]][new_coord[0]][0] == 'b':   # jeżeli czarny pion to moze bić w gore lewo
+                if board[new_coord[1]][new_coord[0]][0] == 'b':  # jeżeli czarny pion to moze bić w gore lewo
                     pawn_moves.extend(sum_directions(N, W))
                 new_coord = sum_directions(self.piece_coord, (sum_directions(N, E)))
-                if board[new_coord[1]][new_coord[0]][0] == 'b':   # jeżeli czarny pion to moze bić w gore prawo
+                if board[new_coord[1]][new_coord[0]][0] == 'b':  # jeżeli czarny pion to moze bić w gore prawo
                     pawn_moves.extend(sum_directions(N, E))
                 return pawn_moves
-            elif self.piece_color == 'b':                   #  to samo dla czarnego piona
+            elif self.piece_color == 'b':  # to samo dla czarnego piona
                 black_pawn_starting_row = 1
                 pawn_moves = S
                 if self.piece_coord[1] == black_pawn_starting_row:  # Jezeli pionek znajduje sie w rzedzie startowym
@@ -45,17 +47,19 @@ class Pawn(Piece):
                 return pawn_moves
 
         self.moves_list = listing_pawn_moves()
-        #TO DO
+        # TO DO
         # bicie w przelocie
 
+
 class King(Piece):
-    def __init__(self, piece_selected, piece_coord): # czy ja potrzebuje tych argumentow
+    def __init__(self, piece_selected, piece_coord):  # czy ja potrzebuje tych argumentow
         super().__init__(piece_selected, piece_coord)
         self.movement_range = 1
-        self.moves_list = (N, S, E, W,
-                           sum_directions(N, E), sum_directions(N, W), sum_directions(S, W), sum_directions(S, W))
+        self.moves_list = N, S, E, W, \
+                          sum_directions(N, E), sum_directions(N, W), sum_directions(S, W), sum_directions(S, W)
         # TO DO
         # dodac znaczniki roszad dla bialego i czarnego
+
 
 class Rook(Piece):
     def __init__(self, piece_selected, piece_coord):
@@ -64,13 +68,16 @@ class Rook(Piece):
         self.movement_range = GRID_SIZE
         self.moves_list = (N, S, E, W)
 
+
 class Knight(Piece):
     def __init__(self, piece_selected, piece_coord):
         super().__init__(piece_selected, piece_coord)
 
         self.movement_range = GRID_SIZE
-        self.moves_list = (sum_directions(N, E))
-
+        self.moves_list = sum_directions(N, N, E), sum_directions(N, N, W), \
+                          sum_directions(E, E, N), sum_directions(E, E, S), \
+                          sum_directions(W, W, N), sum_directions(W, W, S), \
+                          sum_directions(S, S, E), sum_directions(S, S, W)
 
 # MOVEMENT ={
 #     'wP': (UP, (0, -2)),
@@ -145,46 +152,46 @@ class Knight(Piece):
 #                 return moves_allowed
 #         pass
 
-        # self.wK = (DIRECTIONS.values())
-            # castling = (2,0)(1,0)
+# self.wK = (DIRECTIONS.values())
+# castling = (2,0)(1,0)
 
-        
-        # if piece = king
-        #     long_castling_flag = False
-        #     short_castling_flag = False
 
-        #     long_castling_flag = True
-        #     short_castling_flag = True
+# if piece = king
+#     long_castling_flag = False
+#     short_castling_flag = False
 
-        #     def castling ():
-        #         if move == (2,0):
-        #             board(7,7) = '--'
-        #             board(6,7) = self.king()
-        #             board(5,7) = self.rook()
-        #             rook = (king.coord[0]-1)
-        #         else:
-        #             board(0,7) = '--'
-        #             board(2,7) = self.king()
+#     long_castling_flag = True
+#     short_castling_flag = True
 
-        # self.knight_moves():
-        #     if abs(move)==3 
-        #         pass
-        #     else:
-        #         pass
-        #     # moves_list (1,2) (-1,2) (1,-2)(-1,-2) (-2, 1)(-2,-1) (2, 1) (2, -1)
-        # self.bishop()# NIEDOROBIONE
-        #     if sum(coord) % 2 != 0 :
-        #         if move [row] == move [col] or sum(move) == sum(coord)
-        #         return moves_allowed
-        #     else:
-        #         if move [row] == move [col] or sum(move) == sum(coord)
-            
-        # self.rook ()
-        #     if (abs(0,move[1])) or (abs(move[0],0)):
-        #         pass 
-        # self.queen()
-        #     self.rook()
-        #     self.bishop()
-        # self.king()
-        #     self.queen
-        # pass
+#     def castling ():
+#         if move == (2,0):
+#             board(7,7) = '--'
+#             board(6,7) = self.king()
+#             board(5,7) = self.rook()
+#             rook = (king.coord[0]-1)
+#         else:
+#             board(0,7) = '--'
+#             board(2,7) = self.king()
+
+# self.knight_moves():
+#     if abs(move)==3
+#         pass
+#     else:
+#         pass
+#     # moves_list (1,2) (-1,2) (1,-2)(-1,-2) (-2, 1)(-2,-1) (2, 1) (2, -1)
+# self.bishop()# NIEDOROBIONE
+#     if sum(coord) % 2 != 0 :
+#         if move [row] == move [col] or sum(move) == sum(coord)
+#         return moves_allowed
+#     else:
+#         if move [row] == move [col] or sum(move) == sum(coord)
+
+# self.rook ()
+#     if (abs(0,move[1])) or (abs(move[0],0)):
+#         pass
+# self.queen()
+#     self.rook()
+#     self.bishop()
+# self.king()
+#     self.queen
+# pass
