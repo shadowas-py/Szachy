e2365522e179146e5b18cbe781883648220f1911
shadowas-py <glas1690@gmail.com>
2021-05-02 03:23:38 +0200

    grafiki figur sa skonfigurowane w klasach tych figur

--- data/pieces.py
+++ data/pieces.py
@@ -57,12 +57,17 @@ def listing_moves_for_the_piece(piece_movement, movement_range, piece_coord):
 class Pawn:
 
     def __init__(self, color): # domyslna ilosc pól o jakie dana figura moze sie poruszac
-        self.piece_color = color
+        self.color = color
         # self.piece_coord = piece_coord
 
         #MOVEMENT
         self.movement_range = 1
-        self.movement = N,S    # testowo do wywalenia
+        self.movement = N, S    # testowo do wywalenia
+
+        if self.color == 'b':
+            self.file_name = 'black_pawn'
+        else:
+            self.file_name = 'white_pawn'
         # Kolizje z innymi bierkami beda liczone gdzie indziej
         # def generating_pawn_moves(board, piece_coord):
         #     if self.piece_color == 'w':
@@ -96,10 +101,15 @@ class Pawn:
 
 class King:
     def __init__(self, color): # parametry do poprawienia
+        self.color = color
         self.movement_range = 1
         self.movement = N, S, E, W, \
                           sum_directions(N, E), sum_directions(N, W), sum_directions(S, W), sum_directions(S, W)
 
+        if self.color == 'b':
+            self.file_name = 'black_king'
+        else:
+            self.file_name = 'white_king'
         # self.all_moves = listing_moves_for_the_piece(self.movement, self.movement_range, piece_coord)
         # TO DO
         # dodac znaczniki i ruch roszady dla bialego i czarnego
@@ -119,31 +129,44 @@ class Rook:
 
 class Knight:
     def __init__(self, color):
-
+        self.color = color
         self.movement_range = 1
         self.movement = sum_directions(N, N, E), sum_directions(N, N, W), \
                           sum_directions(E, E, N), sum_directions(E, E, S), \
                           sum_directions(W, W, N), sum_directions(W, W, S), \
                           sum_directions(S, S, E), sum_directions(S, S, W)
 
+        if self.color == 'b':
+            self.file_name = 'black_knight'
+        else:
+            self.file_name = 'white_knight'
+
         # self.all_moves = listing_moves_for_the_piece(self.movement, self.movement_range, piece_coord)
 
 class Bishop:
     def __init__(self, color):
-
+        self.color = color
         self.movement_range = GRID_SIZE-1
         self.movement = sum_directions(N, E), sum_directions(N, W), \
                           sum_directions(S, E), sum_directions(S, W), \
 
+        if self.color == 'b':
+            self.file_name = 'black_bishop'
+        else:
+            self.file_name = 'white_bishop'
         # self.all_moves = listing_moves_for_the_piece(self.movement, self.movement_range, piece_coord)
 
 class Queen:
     def __init__(self, color):
-
+        self.color = color
         self.movement_range = GRID_SIZE-1
         self.movement = N, S, E, W, sum_directions(N, E), sum_directions(N, W), \
                           sum_directions(S, E), sum_directions(S, W), \
 
+        if self.color == 'b':
+            self.file_name = 'black_queen'
+        else:
+            self.file_name = 'white_queen'
         # self.all_moves = listing_moves_for_the_piece(self.movement, self.movement_range, piece_coord)
 
 
