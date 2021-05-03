3b4b1f4c58d74cd080ea5d5bc2073abcb7a4ae47
shadowas-py <glas1690@gmail.com>
2021-05-03 17:10:38 +0200

    dokonczono podstawowa funkcje generujaca wszystkie ruchy dla wybranego obiektu

--- data/chessboard.py
+++ data/chessboard.py
@@ -3,7 +3,6 @@ from .game_logic import sum_directions, multiply_direction
 
 class GameState:
     def __init__ (self):
-
         self.board = [
             [Rook('b'), Knight('b'), Bishop('b'), Queen('b'), King('b'), Bishop('b'), Knight('b'), Rook('b')],
             [Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b'), Pawn('b')],
@@ -14,7 +13,7 @@ class GameState:
             [Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w'), Pawn('w')],
             [Rook('w'), Knight('w'), Bishop('w'), Queen('w'), King('w'), Bishop('w'), Knight('w'), Rook('w')]]
 
-    def generating_all_moves_for_piece(self, board, piece, coord, active_player='w'):  # WYPISYWANIE KOLEJNYCH KOLUMN
+    def generating_all_moves_for_piece(self, board, piece, coord,):  # WYPISYWANIE KOLEJNYCH KOLUMN
         moves_list = []
         for j in range(len(piece.movement)):
             for i in range(piece.movement_range):
@@ -23,9 +22,12 @@ class GameState:
                 col, row = coords_after_move
                 # if pilnujacy zeby generowane ruchy nie wychodzilo poza zakres planszy
                 if min(coords_after_move) >= 0 and max(coords_after_move) < 8:
-                    "dodac if zapobiega generowaniu sie wspolrzednych poza polem z bierka przeciwnego koloru?"
                     if board[row][col] != None: # jezeli napotka na przeszkode przerywa iteracje po dlugosci ruchu
-                        break
+                        if board[row][col].color == piece.color:
+                            break
+                        else:
+                            moves_list.append(increased_piece_movement)
+                            break
                     else:
                         moves_list.append(increased_piece_movement)
 
