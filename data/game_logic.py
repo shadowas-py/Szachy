a319764064de5dbbb697fb6c97bf5d843d512b46
shadowas-py <glas1690@gmail.com>
2021-05-04 15:18:15 +0200

    odśmiecenie

--- data/game_logic.py
+++ data/game_logic.py
@@ -22,14 +22,6 @@ def get_game_coord_from_mouse():
     else:
         return None
 
-# def selecting_piece(board, coord, active_player): # Zwraca None jeżeli nie jest klikniete pole z figura aktywnego gracza
-#     row, col = coord
-#     piece = board[col][row]
-#     if piece[0] == active_player:
-#         return piece
-#     else:
-#         return None
-
 def selecting_piece(board, coord, active_player): # Zwraca None jeżeli nie jest klikniete pole z figura aktywnego gracza
     row, col = coord
     piece = board[col][row]
@@ -37,13 +29,6 @@ def selecting_piece(board, coord, active_player): # Zwraca None jeżeli nie jest
         if piece.color == active_player:
             return piece
         return None
-
-# def listing_valid_moves(piece_selected, piece_coord,target_content, target_coord):# active_player
-#     if piece_selected[1] == 'P':
-#         valid_moves = valid_pawn_moves(piece_selected, piece_coord, target_content, target_coord)
-#         return valid_moves
-#     else:
-#         pass
 
 # zeby nie wywalalo jak kliknie sie poza plansze
 def making_move(board, piece_selected, base_coord, target_coord, moves_list):
