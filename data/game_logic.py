2866e26c5c35cd9286bd22d19bcfa389231aca59
shadowas-py <glas1690@gmail.com>
2021-05-04 15:15:44 +0200

    skonczona funkcja making move i funkcja odejmowania tupli- Generuje podstawowe ruchy wszystkich figur za wyjatkiem piona.

--- data/game_logic.py
+++ data/game_logic.py
@@ -2,15 +2,17 @@ import pygame
 
 from .constants import BOARD_POSITION, TILE_SIZE, BOARD_END_POSITION
 
+'''moze uzyc numpy'''
 def sum_directions(direction1, direction2, direction3=(0, 0)):
     return tuple(map(sum, zip(direction1, direction2, direction3)))
 
-def sub_directions(direction1, direction2, direction3=(0, 0)):### dokonczyc
-    return tuple(map(sum, zip(direction1, direction2, direction3)))
+def sub_directions(direction1, direction2,):
+    return tuple(direction1 - direction2 for direction1, direction2 in zip(direction1, direction2))
 
 def multiply_direction(direction, multiplier):
     multiplied_direction = (direction[0]*multiplier, direction[1]*multiplier)
     return list(multiplied_direction)
+
 
 def get_game_coord_from_mouse():
     mouse_pos = pygame.mouse.get_pos()
@@ -45,8 +47,7 @@ def selecting_piece(board, coord, active_player): # Zwraca None jeżeli nie jest
 
 # zeby nie wywalalo jak kliknie sie poza plansze
 def making_move(board, piece_selected, base_coord, target_coord, moves_list):
-    piece_shift = sum_directions(base_coord, target_coord)
-    '''DOKONCZYC'''
+    piece_shift = list(sub_directions(target_coord, base_coord))
     if piece_shift in moves_list:#
         board[base_coord[1]][base_coord[0]] = None
         board[target_coord[1]][target_coord[0]] = piece_selected
