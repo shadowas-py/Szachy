b7afa4bee8115e0c15cd38b9de89c6edfcca1b74
Igor Zimny <ingwarusDev@gmail.com>
2021-08-10 10:43:08 +0200

    skrócenie funkcji dla zabawy. zwróć uwagę na komentarze i usuń jak przetworzysz

--- data/display_info.py
+++ data/display_info.py
@@ -1,17 +1,11 @@
 # FUNKCJA DO PROWADZENIA ZAPISU PARTII
 
-def translate_to_chess_notation(*args, trace = False):
-    y_coord_dict = {0: '8', 1: '7', 2: '6', 3: '5', 4: '4', 5: '3', 6: '2', 7: '1'}
-    x_coord_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H'}
+coordMap = lambda it: chr(65+it[0])+str(8-it[1])
+def translate_to_chess_notation(*movesLists, trace = False): # IZ: Po co jest to trace?
     print('MOZLIWE RUCHY NA POLA:')
-    for arg in args:
-        for coord in arg:
-            try:
-                print(x_coord_dict[coord[0]], y_coord_dict[coord[1]], sep='', end=' ')
-            except KeyError:
-                print(x_coord_dict[coord[0][0]], y_coord_dict[coord[0][1]], sep='', end='>>')
-                print(x_coord_dict[coord[1][0]], y_coord_dict[coord[1][1]],sep='', end=' ')
-
+    for movesList in movesLists:
+        print(' '.join(map(lambda it: coordMap(it[0])+">>"+coordMap(it[1]) if type(it[0]) is tuple else coordMap(it), movesList)))
+    # IZ: Twoja funkcja powinna na koncu printować nową linię. inaczej następny print zacznie pisać od tej samej!
 
 if __name__ == '__main__':
     # translate_to_chess_notation((0,0, 1,7),(5,5),(1,1),(3,4))
