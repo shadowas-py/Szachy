99671862c3962f8f35a8fd2d16d471db0a73d547
shadowas-py <81883796+shadowas-py@users.noreply.github.com>
2021-10-31 15:25:26 +0100

    gracze maja liste swoich figur

--- data/players.py
+++ data/players.py
@@ -1,3 +1,4 @@
+from data.constants import GRID_SIZE
 # players_color_list = ['w','b']
 #
 # def setting_instances_global_names(cls, how_many, *args):
@@ -16,9 +17,16 @@ class Player:
     def __init__(self, color):
         self.color = color
         self.time = ...
-        self.pins = {} # attacker: defender
-        self.attacked_tiles_in_pin = {} # attacker: all tiles between attacker and enemy king
+        self.pins = {}  # attacker: defender
+        self.attacked_tiles_in_pin = {}  # attacker: all tiles between attacker and enemy king
         self.checks = {}
         self.attacked_tiles_in_check = {}
         self.all_attacked_tiles = []
+        self.all_possible_possible_moves = ...
+
+    def pieces_list(self, game):
+        for col in range(GRID_SIZE):
+            for row in range(GRID_SIZE):
+                if game.board[row][col] and game.board[row][col].color == self.color:
+                    yield game.board[row][col]
 
